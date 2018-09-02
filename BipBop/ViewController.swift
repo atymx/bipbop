d257ca33d21b2d82225b9fa52c1436af6655afe8
Максим Атюцкий <mxm_at@macbook-pro-maksim.local>
2018-09-02 18:46:16 +0300

    Initial Commit

---
+++ BipBop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BipBop
+//
+//  Created by Максим Атюцкий on 02.09.2018.
+//  Copyright © 2018 Atyutskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
